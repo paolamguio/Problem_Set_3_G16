0ce8aca871c43500f4b35d2ab0f73540d724fa57
paolamguio <ap.morales1967@uniandes.edu.co>
2022-07-24 12:40:12 -0500

    Create 3. estad_descr.R

---
+++ 2. Scripts/3. estad_descr.R
@@ -0,0 +1,112 @@
+
+# Estadísticas descriptivas
+# Problem_Set_3 
+# Grupo 16
+# Andres Martinez, Paola Morales y Oscar Cortes 
+--------------------------------------------------
+  
+## preparación del espacio
+rm(list = ls())
+setwd("C:/Users/amorales/OneDrive - ANI/Documentos/GitHub/Problem_Set_3_G16/3. Stores")
+
+## llamado librerías de la sesión
+require(pacman)
+
+p_load(tidyverse,
+  rvest,
+  writexl,
+  rio,
+  skimr,
+  sf,
+  pastecs,
+  stargazer,
+  PerformanceAnalytics,
+  naniar
+)
+
+##### Estadísticas descriptivas base train #####
+
+## se importan bases de datos creada en 1.data_cleaning
+df_hogares<- readRDS("df_house_mnz.rds")
+class(df_hogares)
+dim(df_hogares)
+colnames(df_hogares)
+
+table(df_hogares$Neighborhood == "Poblado")
+
+sum(df_hogares$Neighborhood)
+
+table(is.na(df_hogares$Neighborhood))
+
+summary(is.na(df_hogares$Neighborhood==T))
+        
+df_hogares$Neighborhood
+
+# separación de base de datos
+
+df_trainCH <- df_hogares %>% subset(base == "train" & Neighborhood == "Chapinero") 
+df_trainP <- df_hogares %>% subset(base == "train" & Neighborhood == "Poblado") 
+
+df_testCH <- df_hogares %>% subset(base == "test" & Neighborhood == "Chapinero") 
+df_testP <- df_hogares %>% subset(base == "test" & Neighborhood == "Poblado") 
+
+### 1. estadísticas descriptivas ###
+summary(df_trainCH)
+summary(df_trainP)
+summary(df_testCH)
+summary(df_testP)
+
+st_drop_geometry(df_trainCH)
+
+# estadísticas descriptivas variables de interes
+require(gtsummary) #llamado librería
+
+# estadísiticas descriptivas generales datos
+
+stat.desc(df_trainCH)
+descriptivastch <- stat.desc(df_trainCH) # se guardan las estadísticas descriptivas de todas las variables para luego exportarlas a un excel
+descriptivastch$Estadisticas <- row.names(descriptivastch) # se crea columna dentro del dataframe con el nombre de las filas 
+descriptivastch <- descriptivastch %>% select(Estadisticas, everything()) # se ubica la columna creada en la primera posición 
+write_xlsx(descriptivastch, "d_train_ch.xlsx") # se exporta a excel tabla con las estadísticas descriptivas
+
+stat.desc(df_trainP)
+descriptivastp <- stat.desc(df_trainP)
+descriptivastp$Estadisticas <- row.names(descriptivastp) 
+descriptivastp <- descriptivastp %>% select(Estadisticas, everything())
+write_xlsx(descriptivastp, "d_train_p.xlsx")
+
+stat.desc(df_testCH)
+descriptivasch <- stat.desc(df_testCH) # se guardan las estadísticas descriptivas de todas las variables para luego exportarlas a un excel
+descriptivasch$Estadisticas <- row.names(descriptivasch) # se crea columna dentro del dataframe con el nombre de las filas 
+descriptivasch <- descriptivasch %>% select(Estadisticas, everything()) # se ubica la columna creada en la primera posición 
+write_xlsx(descriptivasch, "d_test_CH.xlsx") # se exporta a excel tabla con las estadísticas descriptivas
+
+stat.desc(df_testP)
+descriptivasp <- stat.desc(df_testP) # se guardan las estadísticas descriptivas de todas las variables para luego exportarlas a un excel
+descriptivasp$Estadisticas <- row.names(descriptivasp) # se crea columna dentro del dataframe con el nombre de las filas 
+descriptivasp <- descriptivasp %>% select(Estadisticas, everything()) # se ubica la columna creada en la primera posición 
+write_xlsx(descriptivasp, "d_test_p.xlsx") # se exporta a excel tabla con las estadísticas descriptivas
+
+
+
+
+
+
+
+# Tablas descriptivas
+
+
+
+df_trainCH1 <- st_set_geometry(df_trainCH, NULL)
+
+tbl_summary(df_trainCH, by= property_id, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
+
+df_trainCH1 <- df_trainCH %>% select(bedrooms, bathrooms, property_type, parking, ascensor, balcon, terraza, remodelado, estrato)
+table1 <- tbl_summary(df_trainCH1)
+
+tbl_summary(df_trainCH1, statistic = list, (all_continuous()~"{mean} ({sd})")) # generales
+tbl_summary(df_trainCH1, by= property_id, statistic = list ("parking, ascensor, balcon, terraza, remodelado, estrato" ~"{mean} ({sd})")) # por clasificación
+
+
+tbl_summary(df, by= Pobre, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
+
